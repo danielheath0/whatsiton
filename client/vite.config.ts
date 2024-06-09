1ab9aefb865913e19120e525129f5ffe719b56b6
danielheath0 <daniel.heath0@gmail.com>
2024-06-09 20:39:24 +0300

    vite env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: (import.meta as any).env.PORT ? Number((import.meta as any).env.PORT) : 3000,
+  }
 })
