9a7999d197ff69f630c08a4316b17e7145cd1f58
danielheath0 <daniel.heath0@gmail.com>
2024-06-10 02:23:11 +0300

    fixed const error

--- client/src/features/shows/SearchForm.tsx
+++ client/src/features/shows/SearchForm.tsx
@@ -4,28 +4,32 @@ import { useAppDispatch } from "../../app/store";
 import { SearchFormProps } from "../../interfaces/interfaces";
 import { useSelector } from "react-redux";
 import { userCountry,  } from "../users/usersSlice";
-
 const SearchForm: React.FC<SearchFormProps> = () => {
   const dispatch = useAppDispatch();
+
+
+  // const state = useSelector((state) => state);
+  // console.log(state)
+
+const country = useSelector(userCountry)
+// console.log("country:",country)
+console.log("country:",country)
 
   const [title, setTitle] = useState("");
 
   const [searchStatus, setSearchStatus] = useState("idle");
-
   const onTitleChanged = (e: React.FormEvent<HTMLInputElement>) =>
     setTitle(e.currentTarget.value);
  
-
   const canSearch =
     [title].every(Boolean) && searchStatus === "idle";
-
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const onSearchClicked = (e:any) => {
     e.preventDefault()
-    console.log(country)
+    // console.log(country)
     console.log(country)
     if (canSearch) {
-        console.log("Search button clicked")
+        // console.log("Search button clicked")
         console.log("Search button clicked")
       setSearchStatus("pending");
       dispatch(fetchShows({ title, country }));
