fc338de19f2dc1a696c01a3ffa799d5eb88da335
danielheath0 <daniel.heath0@gmail.com>
2024-06-10 02:34:47 +0300

    chore: Remove commented dotenv import in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
